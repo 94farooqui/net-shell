f9f672bf85d1d69ce542f99ea17ee9d3131013f2
94farooqui <94.farooqui@gmail.com>
2025-02-06 06:40:21 +0400

    hosts filtering based on name and IP working fine

--- frontend/src/pages/Hosts.jsx
+++ frontend/src/pages/Hosts.jsx
@@ -6,46 +6,38 @@ import NewHost from "./NewHost";
 import { useNavigate } from "react-router-dom";
 import HostGroupCard from "../components/HostGroupCard";
 
-// Sample data (Replace with API call in real implementation)
-// const hosts2 = [
-//   { id: 1, name: "Router-1", ipAddress: "10.10.1.1", type: "router", connectionMethod: "ssh-key", lastConnected: "2025-01-20" },
-//   { id: 2, name: "Switch-2", ipAddress: "10.10.2.1", type: "switch", connectionMethod: "password", lastConnected: "2025-01-19" },
-//   { id: 3, name: "Firewall", ipAddress: "10.10.3.1", type: "firewall", connectionMethod: "none", lastConnected: "2025-01-15" },
-//   { id: 4, name: "Linux Server", ipAddress: "10.10.4.1", type: "server", connectionMethod: "ssh-key", lastConnected: "2025-01-21" },
-//   { id: 5, name: "Router-1", ipAddress: "10.10.1.1", type: "router", connectionMethod: "ssh-key", lastConnected: "2025-01-20" },
-//   { id: 6, name: "Switch-2", ipAddress: "10.10.2.1", type: "switch", connectionMethod: "password", lastConnected: "2025-01-19" },
-//   { id: 7, name: "Firewall", ipAddress: "10.10.3.1", type: "firewall", connectionMethod: "none", lastConnected: "2025-01-15" },
-//   { id: 8, name: "Linux Server", ipAddress: "10.10.4.1", type: "server", connectionMethod: "ssh-key", lastConnected: "2025-01-21" },
-//   { id: 9, name: "Router-1", ipAddress: "10.10.1.1", type: "router", connectionMethod: "ssh-key", lastConnected: "2025-01-20" },
-//   { id: 10, name: "Switch-2", ipAddress: "10.10.2.1", type: "switch", connectionMethod: "password", lastConnected: "2025-01-19" },
-//   { id: 11, name: "Firewall", ipAddress: "10.10.3.1", type: "firewall", connectionMethod: "none", lastConnected: "2025-01-15" },
-//   { id: 12, name: "Linux Server", ipAddress: "10.10.4.1", type: "server", connectionMethod: "ssh-key", lastConnected: "2025-01-21" },
-//   { id: 13, name: "Router-1", ipAddress: "10.10.1.1", type: "router", connectionMethod: "ssh-key", lastConnected: "2025-01-20" },
-//   { id: 14, name: "Switch-2", ipAddress: "10.10.2.1", type: "switch", connectionMethod: "password", lastConnected: "2025-01-19" },
-//   { id: 15, name: "Firewall", ipAddress: "10.10.3.1", type: "firewall", connectionMethod: "none", lastConnected: "2025-01-15" },
-//   { id: 16, name: "Linux Server", ipAddress: "10.10.4.1", type: "server", connectionMethod: "ssh-key", lastConnected: "2025-01-21" },
-//   { id: 17, name: "Router-1", ipAddress: "10.10.1.1", type: "router", connectionMethod: "ssh-key", lastConnected: "2025-01-20" },
-//   { id: 18, name: "Switch-2", ipAddress: "10.10.2.1", type: "switch", connectionMethod: "password", lastConnected: "2025-01-19" },
-//   { id: 19, name: "Firewall", ipAddress: "10.10.3.1", type: "firewall", connectionMethod: "none", lastConnected: "2025-01-15" },
-//   { id: 20, name: "Linux Server", ipAddress: "10.10.4.1", type: "server", connectionMethod: "ssh-key", lastConnected: "2025-01-21" },
-//   { id: 21, name: "Router-1", ipAddress: "10.10.1.1", type: "router", connectionMethod: "ssh-key", lastConnected: "2025-01-20" },
-//   { id: 22, name: "Switch-2", ipAddress: "10.10.2.1", type: "switch", connectionMethod: "password", lastConnected: "2025-01-19" },
-//   { id: 23, name: "Firewall", ipAddress: "10.10.3.1", type: "firewall", connectionMethod: "none", lastConnected: "2025-01-15" },
-//   { id: 24, name: "Linux Server", ipAddress: "10.10.4.1", type: "server", connectionMethod: "ssh-key", lastConnected: "2025-01-21" },
-// ];
 
-const handleSearchInputChange = (e) => {};
-
-const handleSearch = (e) => {};
 
 const Hosts = () => {
+  const navigate = useNavigate();
   const token = localStorage.getItem("net_shell_token");
   //console.log(token);
-  const [groupNames, setGroupNames] = useState([]);
+
+  const [searchFilter, setSearchFilter] = useState("")
   const [hostGroups, setHostGroups] = useState([]);
+  const [filteredHostGroups, setFilteredHostGroups] = useState([])
+
+  const [groupNames, setGroupNames] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const navigate = useNavigate();
+  useEffect(() => {
+    const filteredGroup = hostGroups.map(group => ({ ...group, devices: group.devices.filter(device => (device.name.toLowerCase().includes(searchFilter) || device.ipAddress.includes(searchFilter))) })).filter(group => group.devices.length > 0)
+    if (filteredGroup) {
+      console.log("found Group", filteredGroup)
+      setFilteredHostGroups(filteredGroup)
+    }
+  }, [searchFilter])
+
+  const handleSearchInputChange = (e) => {
+    const searchValue = e.target.value.toLocaleLowerCase()
+    console.log(searchValue)
+    setSearchFilter(searchValue)
+
+    if (!searchValue) {
+      setFilteredHostGroups(hostGroups)
+    }
+  };
+
 
   const getHosts = async () => {
     const response = await axios.get("http://localhost:5000/api/hosts", {
@@ -55,8 +47,13 @@ const Hosts = () => {
     });
     if (response.status == 200) {
       //console.log(response.data);
-      setGroupNames(response.data.map(group => ({id:group._id, name:group.name})))
+
+
+      setGroupNames(response.data.map(group => ({ id: group._id, name: group.name })))
+
       setHostGroups(response.data);
+      setFilteredHostGroups(response.data)
+
       setLoading(false);
     }
 
@@ -89,24 +86,20 @@ const Hosts = () => {
       <div className="mb-6">
         <form
           className="w-full flex gap-x-4"
-          onSubmit={handleSearch}
-          onChange={handleSearchInputChange}
         >
           <input
+            onChange={handleSearchInputChange}
             className="flex-1 p-2 rounded-md border bg-gray-800  border-gray-700 focus:outline-none focus:border-gray-500"
             placeholder="Enter IP or Host name"
           />
-          <button className="bg-blue-600 text-white px-4 py-2 rounded-md flex items-center gap-2 hover:bg-blue-700 transition">
-            Search
-          </button>
         </form>
       </div>
 
       {/* Hosts Grid */}
       <div className="flex flex-col">
-      {hostGroups.map((hostGroup) => (
-        <HostGroupCard key={hostGroup._id} hostGroup={hostGroup} groupNames={groupNames} refetchHosts={getHosts} />
-      ))}
+        {filteredHostGroups.map((hostGroup) => (
+          <HostGroupCard key={hostGroup._id} hostGroup={hostGroup} groupNames={groupNames} refetchHosts={getHosts} />
+        ))}
       </div>
       {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {hosts.map((host) => (
